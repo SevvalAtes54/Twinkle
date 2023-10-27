a211e1662d7b74fbe0af184e2faf9d60ba58a681
ŞEVVAL ATEŞ <116071942+SevvalAtes54@users.noreply.github.com>
2023-10-27 13:35:40 +0300

    Add files via upload

--- ChooseGenderVC.swift
+++ ChooseGenderVC.swift
@@ -8,6 +8,7 @@
 import UIKit
 import TinyConstraints
 class ChooseGenderVC: UIViewController {
+    var choosed = false
     let labelQuestion : UITextView = {
         let cons = UITextView()
         cons.translatesAutoresizingMaskIntoConstraints = false
@@ -44,10 +45,16 @@ class ChooseGenderVC: UIViewController {
 //        cons.layer.cornerRadius = 14
         return cons
     }()
+    let chooseAsign : UILabel = {
+        let cons = UILabel()
+        cons.translatesAutoresizingMaskIntoConstraints = false
+//        cons.layer.cornerRadius = 14
+        return cons
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         let backgroundImageView = UIImageView()
         backgroundImageView.image = UIImage(named: "mainImage")
         backgroundImageView.contentMode = .scaleAspectFill
@@ -60,32 +67,45 @@ class ChooseGenderVC: UIViewController {
         view.addSubview(manButton)
         view.addSubview(nonbinaryButton)
         view.addSubview(nextButton)
+        view.addSubview(chooseAsign)
+        
+       
         
         
         labelQuestion.topToSuperview(offset:120)
         labelQuestion.text = "What is your gender"
         labelQuestion.leadingToSuperview(offset:55)
         labelQuestion.trailingToSuperview(offset:55)
+        //        labelQuestion.centerXToSuperview()
         labelQuestion.font = .boldSystemFont(ofSize: 35)
         labelQuestion.tintColor = .white
-        labelQuestion.width(140)
-        labelQuestion.height(80)
+        //        labelQuestion.width(350)
+        labelQuestion.height(130)
         labelQuestion.backgroundColor = .clear
         
         
-        labelDescript.topToBottom(of: labelQuestion,offset:30)
-        labelDescript.text = "gender menaing gender gender gender genderrrrrr"
+        labelDescript.top(to: labelQuestion,offset:100)
+        labelDescript.text = "Your gender refer to how you identify yourself."
         labelDescript.leadingToSuperview(offset:55)
         labelDescript.trailingToSuperview(offset:55)
         labelDescript.font = .systemFont(ofSize: 20)
         labelDescript.tintColor = .white
-        labelDescript.width(150)
+        //        labelDescript.width(150)
         labelDescript.height(80)
         labelDescript.backgroundColor = .clear
         
+        chooseAsign.top(to: labelDescript,offset: 100)
+        chooseAsign.centerXToSuperview()
+        chooseAsign.textColor = .white
+        chooseAsign.text = "Please choose a sign"
+        chooseAsign.isHidden = true
+        chooseAsign.font = .systemFont(ofSize: 20)
+        
         womanButton.leadingToSuperview(offset: 55)
         womanButton.trailingToSuperview(offset: 55)
-        womanButton.topToBottom(of: labelDescript,offset:35)
+        womanButton.top(to: labelDescript,offset:170)
+        womanButton.height(50)
+        
         womanButton.backgroundColor = .white
         womanButton.setTitle("  Woman", for: .normal)
         womanButton.setTitleColor(.black, for: .normal)
@@ -94,7 +114,8 @@ class ChooseGenderVC: UIViewController {
         
         manButton.leadingToSuperview(offset: 55)
         manButton.trailingToSuperview(offset: 55)
-        manButton.topToBottom(of: womanButton,offset:35)
+        manButton.top(to: womanButton,offset:70)
+        
         manButton.backgroundColor = .white
         manButton.setTitle("  Man", for: .normal)
         manButton.setTitleColor(.black, for: .normal)
@@ -103,7 +124,8 @@ class ChooseGenderVC: UIViewController {
         
         nonbinaryButton.leadingToSuperview(offset: 55)
         nonbinaryButton.trailingToSuperview(offset: 55)
-        nonbinaryButton.topToBottom(of: manButton,offset:35)
+        nonbinaryButton.top(to: manButton,offset:70)
+        
         nonbinaryButton.backgroundColor = .white
         nonbinaryButton.setTitle("  Non Binary", for: .normal)
         nonbinaryButton.setTitleColor(.black, for: .normal)
@@ -111,33 +133,64 @@ class ChooseGenderVC: UIViewController {
         nonbinaryButton.addTarget(self, action: #selector(nonbinary), for: .touchUpInside)
         
         
+        
+        
         nextButton.bottomToSuperview(offset:-30)
-        nextButton.trailingToSuperview(offset:30)
-        nextButton.setImage(UIImage(systemName: "arrowshape.right"), for: .normal)
-        nextButton.height(30)
-        nextButton.width(30)
+        nextButton.trailingToSuperview(offset:25)
+        nextButton.setImage(UIImage(named: "arrow"), for: .normal)
+        nextButton.height(40)
+        nextButton.width(45)
+        nextButton.addTarget(self, action: #selector(nextButtonNext), for: .touchUpInside)
         
     }
-    @objc func woman (){
-        print("woamnn")
+       
+       @objc func woman (){
+            choosed = true
+           resetButtonColors()
+           chooseAsign.isHidden = true
+           womanButton.backgroundColor = .yellow.withAlphaComponent(0.7)
+           
+       }
+      
+       @objc func mann (){
+            choosed = true
+           resetButtonColors()
+           chooseAsign.isHidden = true
+           manButton.backgroundColor = .yellow.withAlphaComponent(0.7)
+           
+       }
+       @objc func nonbinary (){
+            choosed = true
+           resetButtonColors()
+           chooseAsign.isHidden = true
+       
+   nonbinaryButton.backgroundColor = .yellow.withAlphaComponent(0.7)
+          
+       }
+    
+     
+       @objc func resetButtonColors() {
+           nonbinaryButton.backgroundColor = .white
+           manButton.backgroundColor = .white
+           womanButton.backgroundColor = .white
+       }
+    @objc func nextButtonNext() {
+      
+        if choosed == true{
+          
+            let nextViewController = EducationVC()
+            nextViewController.modalPresentationStyle = .fullScreen
+            self.present(nextViewController, animated: true)
+        } else {
+            print("Lütfen bir düğme seçin.")
+            chooseAsign.isHidden = false
+            print( chooseAsign.isHidden)
+            
+            
+        }
+        
     }
-   
-    @objc func mann (){
-        print("mann")
-    }
-    @objc func nonbinary (){
-        print("nonbinary")
-    }
-   
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
+  
 
 }
