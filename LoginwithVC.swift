ed6648e4fc20ed2a3067bb5f157058ef6925184b
ŞEVVAL ATEŞ <116071942+SevvalAtes54@users.noreply.github.com>
2023-10-27 13:02:45 +0300

    Add files via upload

--- LoginwithVC.swift
+++ LoginwithVC.swift
@@ -9,7 +9,7 @@
 import UIKit
 import TinyConstraints
 class LoginwithVC: UIViewController {
-   
+    var choosed = false
     let labelSignIn : UILabel = {
         let cons = UILabel()
         cons.translatesAutoresizingMaskIntoConstraints = false
@@ -40,11 +40,19 @@ class LoginwithVC: UIViewController {
         cons.translatesAutoresizingMaskIntoConstraints = false
         return cons
     }()
-    let imageview : UIImageView = {
+    let imageviewForLogo : UIImageView = {
         let cons = UIImageView()
         cons.translatesAutoresizingMaskIntoConstraints = false
+        cons.layer.cornerRadius = 14
         return cons
     }()
+    let chooseAsign : UILabel = {
+        let cons = UILabel()
+        cons.translatesAutoresizingMaskIntoConstraints = false
+      
+        return cons
+    }()
+    
     let nextButton : UIButton = {
         let cons = UIButton()
         cons.translatesAutoresizingMaskIntoConstraints = false
@@ -55,6 +63,8 @@ class LoginwithVC: UIViewController {
         
         super.viewDidLoad()
         
+        
+        
         //Background designing
         let backgroundImageView = UIImageView()
         backgroundImageView.image = UIImage(named: "mainImage")
@@ -62,19 +72,30 @@ class LoginwithVC: UIViewController {
         view.addSubview(backgroundImageView)
         backgroundImageView.edges(to: view)
         
+        view.addSubview(imageviewForLogo)
         view.addSubview(nextButton)
         view.addSubview(mailLoginButton)
         view.addSubview(phoneLoginButton)
         view.addSubview(facebookLoginButton)
         view.addSubview(googleLoginButton)
-        view.addSubview(imageview)
-        view.addSubview(labelSignIn)
         
-        labelSignIn.bottomToSuperview(offset: -60)
-        labelSignIn.centerXToSuperview()
-        labelSignIn.text = "  I already have an account"
-        labelSignIn.textColor = .white
-        labelSignIn.font = .systemFont(ofSize: 20)
+   view.addSubview(chooseAsign)
+
+        imageviewForLogo.topToSuperview(offset: 180)
+        imageviewForLogo.centerXToSuperview()
+        imageviewForLogo.image = UIImage(named: "TwinkleLogo")
+        imageviewForLogo.width(285)
+        imageviewForLogo.height(130)
+        
+   
+        view.addSubview(chooseAsign)
+            chooseAsign.centerXToSuperview()
+            chooseAsign.top(to: imageviewForLogo,offset: 155)
+            chooseAsign.textColor = .white
+
+        chooseAsign.text = "Please choose a sign"
+        chooseAsign.isHidden = true
+        chooseAsign.font = .systemFont(ofSize: 20)
         
         phoneLoginButton.leadingToSuperview(offset: 55)
         phoneLoginButton.trailingToSuperview(offset: 55)
@@ -113,43 +134,93 @@ class LoginwithVC: UIViewController {
         googleLoginButton.addTarget(self, action: #selector(googleLogin), for: .touchUpInside)
         
         
+        let googleiconImage = UIImageView()
+        let facebookiconImage = UIImageView()
+        let mailIcon = UIImageView()
+        googleiconImage.image = UIImage(named: "googleicon")
+        facebookiconImage.image = UIImage(named: "facebookicon")
+        mailIcon.image = UIImage(systemName: "envelope")
+        googleLoginButton.addSubview(googleiconImage)
+        googleiconImage.leading(to: googleLoginButton,offset:6)
+        googleiconImage.centerY(to: googleLoginButton)
+        googleiconImage.width(40)
+        googleiconImage.height(40)
+        
+        googleLoginButton.addSubview(facebookiconImage)
+        facebookiconImage.leading(to: facebookLoginButton,offset:7)
+        facebookiconImage.centerY(to: facebookLoginButton)
+        facebookiconImage.width(30)
+        facebookiconImage.height(30)
+        
+        mailLoginButton.addSubview(mailIcon)
+        mailIcon.leading(to: mailLoginButton,offset:6)
+        mailIcon.centerY(to: mailLoginButton)
+        mailIcon.width(40)
+        mailIcon.height(29)
+        
+    
+       
         nextButton.bottomToSuperview(offset:-30)
-        nextButton.trailingToSuperview(offset:30)
-        nextButton.setImage(UIImage(systemName: "arrowshape.right"), for: .normal)
-        nextButton.height(30)
-        nextButton.width(30)
-        googleLoginButton.addTarget(self, action: #selector(nextButtonNext), for: .touchUpInside)
+        nextButton.trailingToSuperview(offset:25)
+        nextButton.setImage(UIImage(named: "arrow"), for: .normal)
+        nextButton.height(40)
+        nextButton.width(45)
+        nextButton.addTarget(self, action: #selector(nextButtonNext), for: .touchUpInside)
     }
     
+ 
+    
     @objc func phoneLogin (){
-//        let nextViewController = ChooseGenderVC()
-//        nextViewController.modalPresentationStyle = .fullScreen
-//        self.present(nextViewController, animated: true)
+         choosed = true
+        resetButtonColors()
+        chooseAsign.isHidden = true
+        phoneLoginButton.backgroundColor = .yellow.withAlphaComponent(0.7)
+        
     }
    
     @objc func mailLogin (){
-//        let nextViewController = ChooseGenderVC()
-//        nextViewController.modalPresentationStyle = .fullScreen
-//        self.present(nextViewController, animated: true)
-        print("mailll")
+         choosed = true
+        resetButtonColors()
+        chooseAsign.isHidden = true
+        mailLoginButton.backgroundColor = .yellow.withAlphaComponent(0.7)
+        
     }
     @objc func facebookLogin (){
-//        let nextViewController = ChooseGenderVC()
-//        nextViewController.modalPresentationStyle = .fullScreen
-//        self.present(nextViewController, animated: true)
-        print("faceee")
+         choosed = true
+        resetButtonColors()
+        chooseAsign.isHidden = true
+     //   facebookLoginButton.backgroundColor = .blue
+// facebookLoginButton.backgroundColor = .yellow.withAlphaComponent(0.7)
+        facebookLoginButton.setTitleColor(.orange, for: .normal)
     }
     @objc func googleLogin (){
-//        let nextViewController = ChooseGenderVC()
-//        nextViewController.modalPresentationStyle = .fullScreen
-//        self.present(nextViewController, animated: true)
-        print("gogggii")
+         choosed = true
+       resetButtonColors()
+        chooseAsign.isHidden = true
+ googleLoginButton.backgroundColor = .yellow.withAlphaComponent(0.7)
     }
-    @objc func nextButtonNext (){
-        let nextViewController = ChooseGenderVC()
-        nextViewController.modalPresentationStyle = .fullScreen
-       self.present(nextViewController, animated: true)
-       
+  
+    @objc func resetButtonColors() {
+        phoneLoginButton.backgroundColor = .white
+        mailLoginButton.backgroundColor = .white
+        facebookLoginButton.setTitleColor(.white, for: .normal)
+        googleLoginButton.backgroundColor = .white
+    }
+    @objc func nextButtonNext() {
+      
+        if choosed == true{
+          
+            let nextViewController = ChooseGenderVC()
+            nextViewController.modalPresentationStyle = .fullScreen
+            self.present(nextViewController, animated: true)
+        } else {
+            print("Lütfen bir düğme seçin.")
+            chooseAsign.isHidden = false
+            print( chooseAsign.isHidden)
+            
+            
+        }
+        
     }
     
 }
